eb1dffdff77c8ac79a3a1f08c074e612cab9121c
rotem-ziv21 <162511267+rotem-ziv21@users.noreply.github.com>
2024-12-11 16:16:29 +0200

    Update location ID to ZP9ZJZ6e7kXwdFYSQYDq

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -10,8 +10,8 @@ router.get('/provider', (req, res) => {
         description: process.env.PROVIDER_DESCRIPTION,
         logoUrl: process.env.PROVIDER_LOGO_URL,
         uniqueName: process.env.PROVIDER_UNIQUE_NAME,
-        paymentUrl: `${process.env.BASE_URL}/payment/process/test-location`,
-        queryUrl: `${process.env.BASE_URL}/payment/query/test-location`
+        paymentUrl: `${process.env.BASE_URL}/payment/process/ZP9ZJZ6e7kXwdFYSQYDq`,
+        queryUrl: `${process.env.BASE_URL}/payment/query/ZP9ZJZ6e7kXwdFYSQYDq`
     });
 });
 
